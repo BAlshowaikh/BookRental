797e31c5cf8d940080c168f174c606f4b501bf70
DaniaSuhail <202203819@student.polytechnic.bh>
2025-03-21 14:26:12 +0300

    login form created

---
+++ FormApp/FormApp/Login.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace FormApp
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            linkLabel1 = new LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 26F, FontStyle.Bold);
+            label1.ForeColor = Color.LightSteelBlue;
+            label1.Location = new Point(475, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(171, 63);
+            label1.TabIndex = 0;
+            label1.Text = "Login";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(33, 20);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(156, 145);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Tahoma", 14F, FontStyle.Bold);
+            label2.ForeColor = Color.MidnightBlue;
+            label2.Location = new Point(329, 137);
+            label2.Name = "label2";
+            label2.Size = new Size(158, 34);
+            label2.TabIndex = 2;
+            label2.Text = "Username";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Tahoma", 14F, FontStyle.Bold);
+            label3.ForeColor = Color.MidnightBlue;
+            label3.Location = new Point(337, 291);
+            label3.Name = "label3";
+            label3.Size = new Size(150, 34);
+            label3.TabIndex = 3;
+            label3.Text = "Password";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(338, 187);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(500, 31);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(338, 368);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(500, 31);
+            textBox2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Tahoma", 11F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.SkyBlue;
+            label4.Location = new Point(417, 510);
+            label4.Name = "label4";
+            label4.Size = new Size(240, 27);
+            label4.TabIndex = 6;
+            label4.Text = "Don't have an account?";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Tahoma", 11F);
+            linkLabel1.ForeColor = Color.SkyBlue;
+            linkLabel1.Location = new Point(663, 510);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(92, 27);
+            linkLabel1.TabIndex = 7;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Register";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonHighlight;
+            ClientSize = new Size(1140, 680);
+            Controls.Add(linkLabel1);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "Login";
+            Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label4;
+        private LinkLabel linkLabel1;
+    }
+}
