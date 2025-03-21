5e1dbfabef4da6825add34e9a4f273eccfad6438
DaniaSuhail <202203819@student.polytechnic.bh>
2025-03-21 15:28:58 +0300

    register page created

---
+++ FormApp/FormApp/Register.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace FormApp
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Register));
+            label1 = new Label();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 26F, FontStyle.Bold);
+            label1.ForeColor = Color.LightSteelBlue;
+            label1.Location = new Point(447, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(248, 63);
+            label1.TabIndex = 0;
+            label1.Text = "Register";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Tahoma", 14F, FontStyle.Bold);
+            label2.ForeColor = Color.MidnightBlue;
+            label2.Location = new Point(369, 160);
+            label2.Name = "label2";
+            label2.Size = new Size(158, 34);
+            label2.TabIndex = 1;
+            label2.Text = "Username";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(156, 145);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Tahoma", 14F, FontStyle.Bold);
+            label3.ForeColor = Color.MidnightBlue;
+            label3.Location = new Point(369, 300);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 34);
+            label3.TabIndex = 3;
+            label3.Text = "Email";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Tahoma", 14F, FontStyle.Bold);
+            label4.ForeColor = Color.MidnightBlue;
+            label4.Location = new Point(369, 430);
+            label4.Name = "label4";
+            label4.Size = new Size(150, 34);
+            label4.TabIndex = 4;
+            label4.Text = "Password";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(369, 197);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(423, 31);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(369, 337);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(423, 31);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(369, 467);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(423, 31);
+            textBox3.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LightSteelBlue;
+            button1.Font = new Font("Tahoma", 10F, FontStyle.Bold);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(533, 566);
+            button1.Name = "button1";
+            button1.Size = new Size(162, 51);
+            button1.TabIndex = 8;
+            button1.Text = "Register";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Register
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonHighlight;
+            ClientSize = new Size(1140, 680);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Register";
+            Text = "Register";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+    }
+}
