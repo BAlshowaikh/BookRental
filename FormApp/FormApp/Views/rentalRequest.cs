16c68c4cbab028e2e9e4235490115106c240355a
Khadija Yasser Alsatrawi <kadijayasser@gmail.com>
2025-03-24 17:44:40 +0300

    Logging> design (uncomplete) rental request> desing & code return recourd> design & code

---
+++ FormApp/FormApp/Views/rentalRequest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using BookRentalObject;
+
+namespace FormApp.Views
+{
+    public partial class rentalRequest : Form
+    {
+        BookRentalDBContext context = new BookRentalDBContext();
+        public rentalRequest()
+        {
+            InitializeComponent();
+        }
+
+        private void rentalRequest_Load(object sender, EventArgs e)
+        {
+            ddlFilterUser.DataSource = context.Users.ToList(); ;
+
+            ddlFilterUser.DisplayMember = "FullName";
+            ddlFilterUser.ValueMember = "UserId";
+            ddlFilterUser.SelectedItem = null;
+
+            RefreshRentalRequestGridview();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshRentalRequestGridview();
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            RefreshRentalRequestGridview();
+        }
+
+        private void btnResetFilter_Click(object sender, EventArgs e)
+        {
+            txtFilterRequestNo.Text = string.Empty;
+            txtFilterRequestNo.Focus();
+
+            ddlFilterUser.SelectedValue = string.Empty;
+            RefreshRentalRequestGridview();
+        }
+
+        private void RefreshRentalRequestGridview()
+        {
+            dgvRentalRequest.DataSource = null;
+            var RequestToShow = context.RentalRequests.AsQueryable();
+
+            if (txtFilterRequestNo.Text != "")
+            {
+                RequestToShow = RequestToShow
+                    .Where(x => x.RequestId == Convert.ToInt32(txtFilterRequestNo.Text));
+            }
+            else if (ddlFilterUser.SelectedValue != null)
+            {
+                RequestToShow = RequestToShow
+                    .Where(x => x.UserId == Convert.ToInt32(ddlFilterUser.SelectedValue.ToString()));
+            }
+
+            dgvRentalRequest.DataSource = RequestToShow.ToList();
+        }
+    }
+}
