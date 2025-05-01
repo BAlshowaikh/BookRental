73a06024f0317560de256ba4e199b03852a9e0c8
Batool Alshowaikh <batoolalshowaikh04@outlook.com>
2025-05-01 23:31:32 +0300

    Add helper.js file + Modify the footer

---
+++ FormApp/WebApp/wwwroot/js/Helpers.js
@@ -0,0 +1,62 @@
+﻿// Function for cards that are clickable (Prevent the parent card from interfering with the child buttons inside it)
+function preventStretchedLinkClick(selector = '.prevent-stretch') {
+    document.querySelectorAll(selector).forEach(function (btn) {
+        btn.addEventListener('click', function (e) {
+            e.stopPropagation();  // Prevents click from reaching the stretched-link
+        });
+    });
+}
+
+
+
+// You can Apply (similar code) if you have a "Serach" functionality whare the user should typefunction setupAutocomplete(inputSelector, suggestionListSelector, url) {
+function setupAutocomplete(inputSelector, suggestionListSelector, url) {
+    $(document).ready(function () {
+        const $input = $(inputSelector);
+        const $suggestions = $(suggestionListSelector);
+
+        // On input event
+        $input.on("input", function () {
+            const query = $input.val();
+
+            if (query.length >= 2) {
+                $.ajax({
+                    url: url,
+                    data: { term: query },
+                    success: function (data) {
+                        $suggestions.empty().hide();
+
+                        if (data.length > 0) {
+                            $.each(data, function (index, item) {
+                                $suggestions.append(
+                                    '<li class="list-group-item list-group-item-action">' + item + '</li>'
+                                );
+                            });
+                            $suggestions.show();
+                        }
+                    },
+                    error: function () {
+                        // Optional error handling
+                    }
+                });
+            } else {
+                $suggestions.empty().hide();
+            }
+        });
+
+        // When a suggestion is clicked
+        $(document).on("click", suggestionListSelector + " li", function () {
+            const selected = $(this).text();
+            $input.val(selected);
+            $suggestions.empty().hide();
+        });
+
+        // Hide suggestions on outside click
+        $(document).click(function (e) {
+            if (!$(e.target).closest(inputSelector).length) {
+                $suggestions.empty().hide();
+            }
+        });
+    });
+}
+
