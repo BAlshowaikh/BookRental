1015c3b49e455484af164ecec98823b45d2e998d
Khadija Yasser Alsatrawi <kadijayasser@gmail.com>
2025-05-04 08:17:50 +0300

    formApp: return record details: IDs

--- FormApp/FormApp/Views/returnRecordDetails.cs
+++ FormApp/FormApp/Views/returnRecordDetails.cs
@@ -212,11 +212,11 @@ namespace FormApp.Views
                     case 3:
                         ExtraChargeRate = Convert.ToDouble(context.ExtraCharges.Where(x => x.ExtraChargesId == 1).FirstOrDefault().ExtraChargeRate.ToString());
                         break;
-                    case 1002:
-                        ExtraChargeRate = Convert.ToDouble(context.ExtraCharges.Where(x => x.ExtraChargesId == 7).FirstOrDefault().ExtraChargeRate.ToString());
+                    case 4:
+                        ExtraChargeRate = Convert.ToDouble(context.ExtraCharges.Where(x => x.ExtraChargesId == 3).FirstOrDefault().ExtraChargeRate.ToString());
                         break;
-                    case 1006:
-                        ExtraChargeRate = Convert.ToDouble(context.ExtraCharges.Where(x => x.ExtraChargesId == 4).FirstOrDefault().ExtraChargeRate.ToString());
+                    case 5:
+                        ExtraChargeRate = Convert.ToDouble(context.ExtraCharges.Where(x => x.ExtraChargesId == 2).FirstOrDefault().ExtraChargeRate.ToString());
                         break;
                 }
             }
