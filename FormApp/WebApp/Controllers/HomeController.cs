28f3295cce58ebd6b83571dbc4b9432ddb7a0986
Khadija Yasser Alsatrawi <kadijayasser@gmail.com>
2025-05-06 22:28:44 +0300

    almost done

--- FormApp/WebApp/Controllers/HomeController.cs
+++ FormApp/WebApp/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
+using BookRentalObject;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
 
 namespace WebApp.Controllers
@@ -8,9 +10,11 @@ namespace WebApp.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly BookRentalDBContext _context;
+        public HomeController(ILogger<HomeController> logger, BookRentalDBContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -27,6 +31,26 @@ namespace WebApp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Dashboard()
+        {
+            var categoryCounts = _context.RentalTransactions
+                .Include(r => r.Book)
+                .ThenInclude(b => b.Category)
+                .GroupBy(x=> x.Book.Category.CategoryName)
+                .Select(g => new
+                {
+                    Category = g.Key,
+                    Count = g.Count()
+                })
+                .OrderByDescending(g => g.Count)
+                .ToList();
+
+            ViewBag.CategoryLabels = categoryCounts.Select(c => c.Category).ToList();
+            ViewBag.CategoryData = categoryCounts.Select(c => c.Count).ToList();
+
+            return View();
         }
     }
 }
