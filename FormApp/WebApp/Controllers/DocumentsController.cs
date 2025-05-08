e4fa649c4852690943901936b2c9a571f19b9f62
Batool Alshowaikh <batoolalshowaikh04@outlook.com>
2025-05-08 21:03:36 +0300

    New scaffolded db + Add Documnet controller

---
+++ FormApp/WebApp/Controllers/DocumentsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookRentalObject;
+
+namespace WebApp.Controllers
+{
+    public class DocumentsController : Controller
+    {
+        private readonly BookRentalDBContext _context;
+
+        public DocumentsController(BookRentalDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Documents
+        public async Task<IActionResult> Index()
+        {
+            var bookRentalDBContext = _context.Documents.Include(d => d.RentalRequest);
+            return View(await bookRentalDBContext.ToListAsync());
+        }
+
+        // GET: Documents/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Documents == null)
+            {
+                return NotFound();
+            }
+
+            var document = await _context.Documents
+                .Include(d => d.RentalRequest)
+                .FirstOrDefaultAsync(m => m.DocumentId == id);
+            if (document == null)
+            {
+                return NotFound();
+            }
+
+            return View(document);
+        }
+
+        // GET: Documents/Create
+        public IActionResult Create()
+        {
+            ViewData["RentalRequestId"] = new SelectList(_context.RentalRequests, "RequestId", "RequestId");
+            return View();
+        }
+
+        // POST: Documents/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DocumentId,UploadDate,Blob,RentalRequestId")] Document document)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(document);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RentalRequestId"] = new SelectList(_context.RentalRequests, "RequestId", "RequestId", document.RentalRequestId);
+            return View(document);
+        }
+
+        // GET: Documents/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Documents == null)
+            {
+                return NotFound();
+            }
+
+            var document = await _context.Documents.FindAsync(id);
+            if (document == null)
+            {
+                return NotFound();
+            }
+            ViewData["RentalRequestId"] = new SelectList(_context.RentalRequests, "RequestId", "RequestId", document.RentalRequestId);
+            return View(document);
+        }
+
+        // POST: Documents/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DocumentId,UploadDate,Blob,RentalRequestId")] Document document)
+        {
+            if (id != document.DocumentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(document);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DocumentExists(document.DocumentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RentalRequestId"] = new SelectList(_context.RentalRequests, "RequestId", "RequestId", document.RentalRequestId);
+            return View(document);
+        }
+
+        // GET: Documents/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Documents == null)
+            {
+                return NotFound();
+            }
+
+            var document = await _context.Documents
+                .Include(d => d.RentalRequest)
+                .FirstOrDefaultAsync(m => m.DocumentId == id);
+            if (document == null)
+            {
+                return NotFound();
+            }
+
+            return View(document);
+        }
+
+        // POST: Documents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Documents == null)
+            {
+                return Problem("Entity set 'BookRentalDBContext.Documents'  is null.");
+            }
+            var document = await _context.Documents.FindAsync(id);
+            if (document != null)
+            {
+                _context.Documents.Remove(document);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DocumentExists(int id)
+        {
+          return _context.Documents.Any(e => e.DocumentId == id);
+        }
+    }
+}
